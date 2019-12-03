1b7e121661f177c165740d4eaa82e740caaf77a8
Juuso Hanhivaara <gr189303@gradia.fi>
2019-12-03 10:50:23 +0200

    forms 10 and start of forms 11

---
+++ Forms10/Forms10/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms10
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> dict;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           dict = new Dictionary<string, string>();
+
+        }
+
+        private void LisaaDictionaryyn(object sender, EventArgs e)
+        {
+            dict.Add((textBox1.Text), textBox2.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dict.ContainsKey(textBox3.Text))
+            {
+                textBox4.Text = dict[textBox3.Text];
+            }
+                
+            
+               // textBox4.Text = dict[textBox3.Text]; .Keys.ElementAt(textBox3.Text)
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
